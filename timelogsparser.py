36f701b174c0a9114984fe0d3e7d811982f66a19
Reginald Eli A. Deinla <aldrineg@localhost.localdomain>
2014-02-02 20:59:00 +0800

    Argparser for supporting subcommands.

--- timelogsparser.py
+++ timelogsparser.py
@@ -1,22 +1,30 @@
 import argparse
 import sys
+from refreshbooks import api
 from timelogs import Timelogs
 
 
-parser = argparse.ArgumentParser(description="Timelogs Processor")
-parser.add_argument('--freshbooks-endpoint', dest='freshbooks_endpoint', type=str, default=None, help='Freshbooks api endpoint')
-parser.add_argument('--freshbooks-token', dest='freshbooks_token', type=str, default=None, help='Freshbooks api token')
-parser.add_argument('files', nargs='*', default=None, help='Freshbooks api endpoint')
+parent_parser = argparse.ArgumentParser(description="Timelogs Processor", add_help=False)
+parent_parser.add_argument('files', nargs='*', help='file sources')
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='subcommand')
+freshbooks_subparser = subparsers.add_parser('freshbooks', parents=[parent_parser])
+freshbooks_subparser.add_argument('--endpoint', dest='endpoint',
+    type=str, required=True, default=None,
+    help='Freshbooks api endpoint')
+freshbooks_subparser.add_argument('--token', dest='token', 
+    type=str, required=True, default=None,
+    help='Freshbooks api token')
+freshbooks_subparser.add_argument('--client-id', dest='client_id', 
+    type=int, required=True, default=None,
+    help='Freshbooks client for invoicing')
+files_subparser = subparsers.add_parser('parse', parents=[parent_parser])
 
-def send_to_freshbooks(timelogs):
+def send_to_freshbooks(timelogs, client):
     print 'sending to freshbooks'
 
 def main(argv):
     args = parser.parse_args(argv)
-    print args
-    # filenames = argv
-    # today = datetime.date.today()
-    # print today
     timelogs = []
     for filename in args.files:
         print 'processing %s' % filename
@@ -26,14 +34,12 @@ def main(argv):
         print '############# START #############'
         t.pretty_print()
         print '############## END ##############'
-    if raw_input("send to freshbooks? ").lower().startswith('y'):
-        if not args.freshbooks_endpoint:
-            print 'please specify freshbooks-endpoint'
-            return
-        if not args.freshbooks_token:
-            print 'please specify freshbooks-token'
-            return
-        send_to_freshbooks(timelogs)
+    if args.subcommand == 'freshbooks':
+        if raw_input("send to freshbooks? ").lower().startswith('y'):
+            client = api.TokenClient(args.endpoint, 
+                args.token, 
+                user_agent='radeinla/timelogparser')
+            send_to_freshbooks(timelogs, client)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
